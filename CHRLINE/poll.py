89cec35afd22a607b697dff1c9a231c1fc2435ce
YinMo <aseekgodroad@gmail.com>
2021-10-09 23:26:32 +0800

    fixed bugs

--- CHRLINE/poll.py
+++ CHRLINE/poll.py
@@ -2,33 +2,33 @@
 import os, sys, threading, time, traceback
 
 class Poll(object):
-    opFuncs = {}
 
     def __init__(self):
         pass
-    def __fetchOps(self, revision, count=100):
-        return self.fetchOps(revision, count)
-    
+
+    def __fetchOps(self, count=100):
+        ops = self.fetchOps(self.revision, count)
+        if 'error' in ops:
+            raise Exception(ops['error'])
+        for op in ops:
+            opType = op[3]
+            if opType != -1:
+                self.setRevision(op[1])
+            yield op
+
     def __execute(self, op, func):
         try:
             func(op, self)
         except Exception as e:
             self.log(traceback.format_exc())
 
-    def addOpFunc(self, opType, func):
-        self.opFuncs[opType] = func
-    
     def setRevision(self, revision):
         self.revision = max(revision, self.revision)
 
     def trace(self, func, isThreading=True):
         while self.is_login:
-            ops = self.__fetchOps(self.revision)
-            if 'error' in ops:
-                raise Exception(ops['error'])
-            for op in ops:
+            for op in self.__fetchOps():
                 if op[3] != 0 and op[3] != -1:
-                    self.setRevision(op[1])
                     if isThreading:
                         _td = threading.Thread(target=self.__execute, args=(op, func))
                         _td.daemon = True
