aa1ea85df65a860bda9966bf63a07002cd799560
YinMo <aseekgodroad@gmail.com>
2022-04-30 15:52:31 +0800

    Update TalkService.py

--- CHRLINE/services/TalkService.py
+++ CHRLINE/services/TalkService.py
@@ -716,7 +716,7 @@ class TalkService():
         })
         try:
             data = self.postPackDataAndGetUnpackRespData(
-                "/P5", sqrd, 5, encType=0, headers=hr, readWith=f"TalkService.{METHOD_NAME}")
+                "/P5", sqrd, 5, encType=0, headers=hr, readWith=f"TalkService.{METHOD_NAME}", timeout=180)
             if data is None:
                 return []
             if 'error' not in data:
@@ -1653,6 +1653,8 @@ class TalkService():
         sqrd = self.generateDummyProtocol('sync', params, 4)
         res = self.postPackDataAndGetUnpackRespData(
             '/SYNC5', sqrd, 5, readWith=f"SyncService.{METHOD_NAME}")
+        if res is None:
+            return []
         operationResponse = self.checkAndGetValue(res, 'operationResponse', 1)
         fullSyncResponse = self.checkAndGetValue(res, 'fullSyncResponse', 2)
         partialFullSyncResponse = self.checkAndGetValue(
